c67e317e13ed9269fc47a472f29edfc1f3a6fff1
owen saavedra <newojunior@gmail.com>
2024-04-11 12:40:10 +0800

    Refinement

--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -1,8 +1,8 @@
 export function getMovieById(id) {
-  const API_KEY = ' a51a9bb0cd5bc1ed985d30c82a20bd57';
+  const KEY = ' a51a9bb0cd5bc1ed985d30c82a20bd57';
 
   return fetch(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language`
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${KEY}&language`
   )
     .then(response => response.json())
     .then(data => {
