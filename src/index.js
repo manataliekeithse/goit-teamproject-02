c52867e61c80f279ea62e9ae7a57b547eef2fae8
owen saavedra <newojunior@gmail.com>
2024-04-12 23:21:49 +0800

    update package.json

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './js/popular-gallery';
-import { loader } from './js/loader';
-
-import { fetch } from './js/fetch-movie.js';
-import { pagination } from './js/pagination';
-import { keyMovieQuery } from './js/keyMovieQuery';
-import './js/btn-modal.js';
-
